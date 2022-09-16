87a7835ee226a6e47416a36dd46df8671daa7113
Mustafa Ilikkan <mustafa.ilikkan@ozguryazilim.com.tr>
2022-09-16 23:51:32 +0300

    complete part1d

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, bad, neutral }) => {
+  const total = good + bad + neutral;
+  const average = (good - bad) / total;
+  const positive = `${(good * 100) / total} %`;
+
+  if (total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
+  );
+};
+
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementGood = () => setGood(good + 1);
+  const incrementNeutral = () => setNeutral(neutral + 1);
+  const incrementBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={incrementGood} text="good" />
+      <Button onClick={incrementNeutral} text="neutral" />
+      <Button onClick={incrementBad} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
+
+export default App;
