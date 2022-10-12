8328b2e141277da9f4c44f31c880751a350b61fa
Mustafa Ilikkan <mustafa.ilikkan@ozguryazilim.com.tr>
2022-10-12 08:57:28 +0300

    complete part3a

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const PORT = 3001;
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  let body = `<p>Phonebook has info for ${persons.length} people</p>`;
+  body = body.concat(`<p>${new Date()}</p>`);
+  res.send(body);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: " number or name is missing" });
+  }
+
+  const nameExists = persons.find((p) => p.name === body.name);
+  if (nameExists) {
+    return res.status(400).json({ error: "name already exists" });
+  }
+
+  const id = Math.ceil(Math.random() * 10000);
+  const person = { ...body, id };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
